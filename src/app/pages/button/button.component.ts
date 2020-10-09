dc90bfbf0b3adf1aca4d6f5baf14f0e3ce378ba4
BijanEisapour <bijanprogrammer@gmail.com>
2020-10-09 19:39:17 +0330

    Buttons playground completed

--- src/app/pages/button/button.component.ts
+++ src/app/pages/button/button.component.ts
@@ -9,26 +9,20 @@ export class ButtonComponent implements AfterViewInit {
 	@ViewChild("playgroundButtons") playgroundButtonsRef: ElementRef;
 
 	public playgroundButtons: Button[] = [];
-	public title = "OUT";
 
 	ngAfterViewInit() {
-		this.playgroundButtons.push(new Button(this.playgroundButtonsRef, "dia-button", "", "Basic"));
-		// this.playgroundButtons[1] = new Button(this.playgroundButtonsRef.nativeElement.getElementsByTagName("a")[0], "dia-button", "");
+		this.playgroundButtons.push(new Button(this.playgroundButtonsRef, false, "dia-flat-button", "dia-primary"));
+		this.playgroundButtons.push(new Button(this.playgroundButtonsRef, true, "dia-flat-button", "dia-primary"));
 	}
 
-	changeButtonType(e, isAnchor: boolean, title: string = "favorite") {
-		console.log(this.playgroundButtons[0]);
-		if (!isAnchor)
-			this.playgroundButtons[0].setType(e.target.value, title);
-		else
-			this.playgroundButtons[1].setType(e.target.value, title);
+	changeButtonType(e) {
+		this.playgroundButtons[0].setType(e.target.value);
+		this.playgroundButtons[1].setType(e.target.value);
 	}
 
-	changeButtonColor(e, isAnchor: boolean) {
-		if (!isAnchor)
-			this.playgroundButtons[0].setColor(e.target.value);
-		else
-			this.playgroundButtons[1].setColor(e.target.value);
+	changeButtonColor(e, disabled: boolean = false) {
+		this.playgroundButtons[0].setColor(e.target.value, disabled);
+		this.playgroundButtons[1].setColor(e.target.value, disabled);
 	}
 }
 
@@ -36,30 +30,43 @@ class Button {
 	public element;
 	public contentElement;
 
-	public constructor(parent: ElementRef, public type: string, public color: string, public title: string, public icon: string = "favorite", public isIcon: boolean = false) {
-		this.element = parent.nativeElement.getElementsByTagName("button")[0];
+	public constructor(parent: ElementRef, public isAnchor: boolean, public type: string, public color: string, public icon: string = "favorite", public isIcon: boolean = false) {
+		this.element = parent.nativeElement.getElementsByTagName(isAnchor ? "a" : "button")[0];
 		this.contentElement = this.element.getElementsByClassName("dia-button-wrapper")[0];
-
-		this.setType("dia-button", "Basic");
 	}
 
-	public setType(_type: string, content: string) {
+	public setType(_type: string) {
 		this.element.classList.remove(this.type);
 		this.type = _type;
-		this.element.classList.add(this.type);
 
 		if (["dia-icon-button", "dia-fab", "dia-mini-fab"].includes(this.type)) {
-			this.isIcon = true;
-			this.contentElement.innerHTML = `<dia-icon role="img" class="dia-icon notranslate material-icons dia-icon-no-color" aria-hidden="true" data-dia-icon-type="font">${content}</dia-icon>`;
+			if (this.isAnchor) {
+				this.type = "dia-button";
+			}
+			else {
+				this.isIcon = true;
+				this.contentElement.innerHTML = `<dia-icon role="img" class="dia-icon notranslate material-icons dia-icon-no-color" aria-hidden="true" data-dia-icon-type="font">favorite</dia-icon>`;
+			}
 		}
-		else {
-			this.contentElement.innerHTML = content;
+		else if (!this.isAnchor) {
+			this.contentElement.innerHTML = "Button";
 		}
+
+		this.element.classList.add(this.type);
 	}
 
-	public setColor(_color: string) {
-		this.element.classList.remove(this.color);
+	public setColor(_color: string, _disabled: boolean) {
+		if (this.isAnchor)
+			return;
+
+		if (this.color)
+			this.element.classList.remove(this.color);
 		this.color = _color;
 		this.element.classList.add(this.color);
+
+		if (_disabled)
+			this.element.setAttribute("disabled", "true");
+		else
+			this.element.setAttribute("disabled", "false");
 	}
 }
