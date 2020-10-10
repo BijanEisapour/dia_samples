eff755c3323c141c2f2d33401c80722b4c78f860
BijanEisapour <bijanprogrammer@gmail.com>
2020-10-10 09:12:44 +0330

    Added theme toggler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: [ './app.component.scss' ]
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+	private readonly darkThemeClassName: string = "dia-dark-theme";
+	private isDark: boolean;
+
+	constructor(private renderer: Renderer2) {
+		this.isDark = false;
+	}
+
+	toggleTheme() {
+		if (this.isDark)
+			this.renderer.removeClass(document.body, this.darkThemeClassName);
+		else
+			this.renderer.addClass(document.body, this.darkThemeClassName);
+
+		this.isDark = !this.isDark;
+	}
+}
